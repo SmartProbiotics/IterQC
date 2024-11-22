19cc97cda3628d43042e85c02f6bfe772d2a2b91
xhb <xhb@10.249.45.94>
2024-11-22 22:01:44 +0800

    main

--- IterQC/kplex.h
+++ IterQC/kplex.h
@@ -44,17 +44,17 @@ void print_heuris_log()
     printf("list triangles time: %.4lf s, strong reduce time: %.4lf s\n", list_triangle_time / 1e6, strong_reduce_time / 1e6);
     printf("total-heuristic-time= %.4lf s, FastHeuris-time= %.4lf s, StrongHeuris-time= %.4lf s\n",
            total_heuris_time / 1e6, FastHeuris_time / 1e6, StrongHeuris_time / 1e6);
-    //printf("lb= %d , FastHeuris-lb= %d \n", lb, FastHeuris_lb);
+    // printf("lb= %d , FastHeuris-lb= %d \n", lb, FastHeuris_lb);
     if (solution.size() >= 2 * paramK - 1)
         assert(solution.size() == lb);
 
     if (g.n <= lb)
     {
         printf("The heuristic solution is the ground truth!\n");
-        //print_solution();
+        // print_solution();
         puts("------------------{whole procedure: kPEX}---------------------");
         printf("ground truth= %u , kPEX time: %.4lf s\n\n", solution.size(), (get_system_time_microsecond() - algorithm_start_time) / 1e6);
-        //exit(0);
+        // exit(0);
     }
 }
 
@@ -68,18 +68,18 @@ void FastHeuris()
     {
 #ifndef NO_SQRT
         lb = g.sqrt_degeneracy(&solution);
-        //printf("sqrt lb= %d, use time %.4lf s\n", lb, t.get_time() / 1e6);
+        // printf("sqrt lb= %d, use time %.4lf s\n", lb, t.get_time() / 1e6);
 #endif
         lb = max(lb, 2 * paramK - 2); // we only care the solutions with at least 2k-1 vertices
         lb = max(lb, g.degeneracy_and_reduce(lb, &solution));
-        //printf("After degeneracy and weak reduce, n= %u , m= %u , lb= %d , use time %.4lf s\n", g.n, g.m / 2, lb, t.get_time() / 1e6);
+        // printf("After degeneracy and weak reduce, n= %u , m= %u , lb= %d , use time %.4lf s\n", g.n, g.m / 2, lb, t.get_time() / 1e6);
         if (lb >= g.n)
         {
             g.n = 0;
             FastHeuris_lb = lb;
             FastHeuris_time = t.get_time();
-            //print_heuris_log();
-            //exit(0);
+            // print_heuris_log();
+            // exit(0);
         }
     }
     FastHeuris_lb = lb;
@@ -90,15 +90,15 @@ void FastHeuris()
         Reduction reduce(&g);
         ui pre_n = g.n;
         reduce.strong_reduce(lb);
-        //printf("Afer strong reduce, n= %u , m= %u, use time %.4lf s\n", g.n, g.m / 2, start_strong_reduce.get_time() / 1e6);
+        // printf("Afer strong reduce, n= %u , m= %u, use time %.4lf s\n", g.n, g.m / 2, start_strong_reduce.get_time() / 1e6);
         strong_reduce_time += start_strong_reduce.get_time();
 
         if (lb >= g.n)
         {
             g.n = 0;
             FastHeuris_time = t.get_time();
-            //print_heuris_log();
-            //exit(0);
+            // print_heuris_log();
+            // exit(0);
         }
     }
     FastHeuris_time = t.get_time();
@@ -128,7 +128,7 @@ void StrongHeuris()
             break;
         }
         extend_lb = g.strong_heuris(lb, solution, time_limit);
-        //printf("%dth-StrongHeuris lb= %d\n", iteration_cnt++, extend_lb);
+        // printf("%dth-StrongHeuris lb= %d\n", iteration_cnt++, extend_lb);
         if (extend_lb <= lb)
             break;
         lb = extend_lb;
@@ -140,15 +140,15 @@ void StrongHeuris()
             Reduction reduce(&g);
             ui pre_n = g.n;
             reduce.strong_reduce(lb);
-            //printf("Afer strong reduce, n= %u , m= %u, use time %.4lf s\n", g.n, g.m / 2, start_strong_reduce.get_time() / 1e6);
+            // printf("Afer strong reduce, n= %u , m= %u, use time %.4lf s\n", g.n, g.m / 2, start_strong_reduce.get_time() / 1e6);
             strong_reduce_time += start_strong_reduce.get_time();
 
             if (lb >= g.n)
             {
                 g.n = 0;
                 StrongHeuris_time = t_extend.get_time();
-                //print_heuris_log();
-                //exit(0);
+                // print_heuris_log();
+                // exit(0);
             }
         }
     }
@@ -168,13 +168,13 @@ void EgoHeuris()
         Reduction reduce(&g);
         ui pre_n = g.n;
         reduce.strong_reduce(lb);
-        //printf("Afer strong reduce, n= %u , m= %u, use time %.4lf s\n", g.n, g.m / 2, start_strong_reduce.get_time() / 1e6);
+        // printf("Afer strong reduce, n= %u , m= %u, use time %.4lf s\n", g.n, g.m / 2, start_strong_reduce.get_time() / 1e6);
         strong_reduce_time += start_strong_reduce.get_time();
         if (lb >= g.n)
         {
             g.n = 0;
-            //print_heuris_log();
-            //exit(0);
+            // print_heuris_log();
+            // exit(0);
         }
     }
 }
@@ -183,12 +183,13 @@ void EgoHeuris()
  * @brief stage-I: we conduct heuristic and preprocessing stage
  * i.e., KPHeuris
  */
-bool use_strong_heuris=true;
+bool use_strong_heuris = true;
 void heuris()
 {
     input_n = g.n;
     FastHeuris();
-    if(use_strong_heuris) StrongHeuris(); // generate n subgraphs and compute larger lb
+    if (use_strong_heuris)
+        StrongHeuris(); // generate n subgraphs and compute larger lb
 }
 
 /**
@@ -196,53 +197,56 @@ void heuris()
  */
 void bnb()
 {
-    
 }
 
-int loop_cnt=0;
-long long heuris_average=0,bnb_average=0;
+int loop_cnt = 0;
+long long heuris_average = 0, bnb_average = 0;
 
-int search(Graph* input_graph,int k,double gama,int UB,bool& is_bad_instance)
-{   
+int search(Graph *input_graph, int k, double gama, int UB, bool &is_bad_instance)
+{
     long long start_time = clock();
-    lb=2*k-2;
+    lb = 2 * k - 2;
     solution.clear();
-    for(int i=0;i<lb;i++) solution.insert(i);
-    is_bad_instance=true;
+    for (int i = 0; i < lb; i++)
+        solution.insert(i);
+    is_bad_instance = true;
     paramK = k;
     // TODO:cut the input_graph
     g.copy_graph(input_graph);
     algorithm_start_time = get_system_time_microsecond();
     heuris();
-    cout<<"true_lb: "<<lb<<"  ";
-    if(lb>=UB){
-        is_bad_instance=false;
+    cout << "true_lb: " << lb << "  ";
+    if (lb >= UB)
+    {
+        is_bad_instance = false;
         return lb;
     }
-    bool fake_effect=false;
+    bool fake_effect = false;
 #ifndef NO_FAKE_LB
-    int fake_lb=(lb+UB)/2;
-    if(k==my_floor((1-gama)*(fake_lb-1)+1)||UB<lb) fake_lb=lb;
-    cout<<"fake_lb:"<<fake_lb<<endl;
-    if(lb<fake_effect) fake_effect=true;
-    lb=fake_lb;
+    int fake_lb = (lb + UB) / 2;
+    if (k == my_floor((1 - gama) * (fake_lb - 1) + 1) || UB < lb)
+        fake_lb = lb;
+    cout << "fake_lb:" << fake_lb << endl;
+    if (lb < fake_effect)
+        fake_effect = true;
+    lb = fake_lb;
 #else
-    int fake_lb=lb;
+    int fake_lb = lb;
 #endif
     solution.clear();
-    //if(lb==UB) maybe we need to let lb=lb-1 so that we can get the true solution but not only its size
-    for(int i=0;i<lb;i++) solution.insert(i);
-    long long heuris_time = clock()-start_time;
-    cout<<"Heuris Time:"<<(double)heuris_time/CLOCKS_PER_SEC<<endl;
-
+    // if(lb==UB) maybe we need to let lb=lb-1 so that we can get the true solution but not only its size
+    for (int i = 0; i < lb; i++)
+        solution.insert(i);
+    long long heuris_time = clock() - start_time;
+    cout << "Heuris Time:" << (double)heuris_time / CLOCKS_PER_SEC << endl;
 
     ///****BNB*****///
-    //puts("start BNB!");
-    start_time=clock();
+    // puts("start BNB!");
+    start_time = clock();
     Graph_reduced *G;
     G = new Graph_reduced_adjacent_list(g);
-    int size_reduced=G->size();
-    cout<<"size_reduced:"<<size_reduced<<endl;
+    int size_reduced = G->size();
+    cout << "size_reduced:" << size_reduced << endl;
 
     Branch branch(*G, lb);
     branch.IE_framework();                        // generate n subgraphs
@@ -252,20 +256,22 @@ int search(Graph* input_graph,int k,double gama,int UB,bool& is_bad_instance)
         for (int v : branch.solution)
             solution.insert(v);
     }
-    long long bnb_time = clock()-start_time;
-    cout<<"BNB Time: "<<(double)(bnb_time)/CLOCKS_PER_SEC<<"s"<<endl;
+    long long bnb_time = clock() - start_time;
+    cout << "BNB Time: " << (double)(bnb_time) / CLOCKS_PER_SEC << "s" << endl;
 
-#ifndef NO_AVG_TIME_ASSUME  
+#ifndef NO_AVG_TIME_ASSUME
     loop_cnt++;
-    heuris_average=heuris_average/loop_cnt*(loop_cnt-1)+heuris_time/loop_cnt;
-    bnb_average=bnb_average/loop_cnt*(loop_cnt-1)+bnb_time/loop_cnt;
-    if(heuris_average > bnb_average) use_strong_heuris=false;
-    else use_strong_heuris=true;
+    heuris_average = heuris_average / loop_cnt * (loop_cnt - 1) + heuris_time / loop_cnt;
+    bnb_average = bnb_average / loop_cnt * (loop_cnt - 1) + bnb_time / loop_cnt;
+    if (heuris_average > bnb_average)
+        use_strong_heuris = false;
+    else
+        use_strong_heuris = true;
 #endif
 
     ///****END*****///
 
-    //delete tmp_g;
-    is_bad_instance=(fake_effect&&solution.size()==fake_lb&&UB!=solution.size());
+    // delete tmp_g;
+    is_bad_instance = (fake_effect && solution.size() == fake_lb && UB != solution.size());
     return solution.size();
 }
--- IterQC/main.cpp
+++ IterQC/main.cpp
@@ -1,189 +1,212 @@
 #include "kplex.h"
 #include <string>
 using namespace std;
-#define LEN_LIMIT (1<<10)
+#define LEN_LIMIT (1 << 10)
 char filename[LEN_LIMIT];
 
-void check_graph(Graph* g){
-    cout<<"Check Graph: "<<"N Anno:"<<g->n<<"  M Anno:"<<g->m<<endl;
-    long long n=g->n,m=g->m;
-    if(n*(n-1)<m){
-        cout<<n*(n-1)<<endl;
-        cout<<"Graph Error!"<<endl;
+void check_graph(Graph *g)
+{
+    cout << "Check Graph: " << "N Anno:" << g->n << "  M Anno:" << g->m << endl;
+    long long n = g->n, m = g->m;
+    if (n * (n - 1) < m)
+    {
+        cout << n * (n - 1) << endl;
+        cout << "Graph Error!" << endl;
         exit(1);
     }
 
-    bool* st=new bool[n];
-    ui* pstart=g->pstart,*edges=g->edge_to;
-    memset(st,0,sizeof(bool)*n);
-    for(int i=0;i<n;i++){
+    bool *st = new bool[n];
+    ui *pstart = g->pstart, *edges = g->edge_to;
+    memset(st, 0, sizeof(bool) * n);
+    for (int i = 0; i < n; i++)
+    {
         vector<int> neig;
-        for(int j=g->pstart[i];j<g->pstart[i+1];j++){
-            int v=edges[j];
-            //cout<<"("<<i<<","<<v<<"),"<<endl;
-            if(v<0||v>=n){
+        for (int j = g->pstart[i]; j < g->pstart[i + 1]; j++)
+        {
+            int v = edges[j];
+            // cout<<"("<<i<<","<<v<<"),"<<endl;
+            if (v < 0 || v >= n)
+            {
                 delete[] st;
-                cout<<"Graph Error! Wrong vertice id"<<endl;
+                cout << "Graph Error! Wrong vertice id" << endl;
                 exit(1);
             }
-            if(st[v]){
+            if (st[v])
+            {
                 delete[] st;
-                cout<<"Graph Error! Multiple edges"<<endl;
+                cout << "Graph Error! Multiple edges" << endl;
                 exit(1);
             }
-            if(v==i){
+            if (v == i)
+            {
                 delete[] st;
-                cout<<"Graph Error! Self Loop"<<endl;
+                cout << "Graph Error! Self Loop" << endl;
                 exit(1);
             }
-            st[v]=true;
+            st[v] = true;
             neig.push_back(v);
             m--;
         }
-        for(auto& v:neig) st[v]=false;
+        for (auto &v : neig)
+            st[v] = false;
     }
     delete[] st;
-    if(m){
-        cout<<"Graph Error!not true edge count"<<endl;
-        cout<<"M announced - True count ="<<m<<endl;
+    if (m)
+    {
+        cout << "Graph Error!not true edge count" << endl;
+        cout << "M announced - True count =" << m << endl;
         exit(1);
     }
-    cout<<"Check : Correct Graph"<<endl;
+    cout << "Check : Correct Graph" << endl;
 }
-Graph* graph,*new_graph;
+Graph *graph, *new_graph;
 
-
-void shrink_graph(MiniGraph& mini_g,int d_limit){
-    int n=mini_g.n;
-    ui* degree = new ui[n],*peel_sequence=new ui[n];
-    int* h=mini_g.h,*ne=mini_g.ne,*e=mini_g.e;
-    ListLinearHeap *heap = new ListLinearHeap(n, n-1);
-    for(ui i = 0;i < n;i ++){
+void shrink_graph(MiniGraph &mini_g, int d_limit)
+{
+    int n = mini_g.n;
+    ui *degree = new ui[n], *peel_sequence = new ui[n];
+    int *h = mini_g.h, *ne = mini_g.ne, *e = mini_g.e;
+    ListLinearHeap *heap = new ListLinearHeap(n, n - 1);
+    for (ui i = 0; i < n; i++)
+    {
         degree[i] = mini_g.get_deg(i);
-        assert(degree[i]<n);
+        assert(degree[i] < n);
     }
-    
-    for(ui i = 0;i < n;i ++) 
+
+    for (ui i = 0; i < n; i++)
         peel_sequence[i] = i;
-    heap->init(n, n-1, peel_sequence, degree);
-    for(ui i = 0;i < n;i ++) {
+    heap->init(n, n - 1, peel_sequence, degree);
+    for (ui i = 0; i < n; i++)
+    {
         ui u, key;
         heap->pop_min(u, key);
-        if(key>=d_limit) break;
+        if (key >= d_limit)
+            break;
         peel_sequence[i] = u;
-        for(int j=h[u];j!=-1;j=ne[j]) heap->decrement(e[j],1);
+        for (int j = h[u]; j != -1; j = ne[j])
+            heap->decrement(e[j], 1);
         mini_g.delete_node(u);
     }
-        
+
     delete heap;
     delete[] degree;
     delete[] peel_sequence;
 
-    //start refresh graph
-    int new_n = mini_g.n,new_m=mini_g.m;
-    if(new_graph!=nullptr) delete new_graph;
-    new_graph=new Graph();
-    ui* pstart=new ui[new_n+1],*edges=new ui[new_m],*deg=new ui[new_n];
-    ui  pstart_idx=0,edges_idx=0;
+    // start refresh graph
+    int new_n = mini_g.n, new_m = mini_g.m;
+    if (new_graph != nullptr)
+        delete new_graph;
+    new_graph = new Graph();
+    ui *pstart = new ui[new_n + 1], *edges = new ui[new_m], *deg = new ui[new_n];
+    ui pstart_idx = 0, edges_idx = 0;
 
-    int* id_map=new int[n],idx=0;
-    memset(id_map,-1,n*sizeof(int));
+    int *id_map = new int[n], idx = 0;
+    memset(id_map, -1, n * sizeof(int));
     vector<int> vset;
-    for(int i=0;i<n;i++)
-        if(!mini_g.is_del(i)) deg[idx]=mini_g.get_deg(i),vset.push_back(i),id_map[i]=idx++;
-    pstart[0]=0;
-    for(int i=1;i<=new_n;i++) pstart[i]=pstart[i-1]+deg[i-1];
-    for(int i=0;i<new_n;i++){
-        int u=vset[i];
-        int pre=edges_idx;
+    for (int i = 0; i < n; i++)
+        if (!mini_g.is_del(i))
+            deg[idx] = mini_g.get_deg(i), vset.push_back(i), id_map[i] = idx++;
+    pstart[0] = 0;
+    for (int i = 1; i <= new_n; i++)
+        pstart[i] = pstart[i - 1] + deg[i - 1];
+    for (int i = 0; i < new_n; i++)
+    {
+        int u = vset[i];
+        int pre = edges_idx;
         assert(!mini_g.is_del(u));
-        for(int j=h[u];j!=-1;j=ne[j]){
-            int v=id_map[e[j]];
+        for (int j = h[u]; j != -1; j = ne[j])
+        {
+            int v = id_map[e[j]];
             assert(!mini_g.is_del(e[j]));
-            edges[edges_idx++]=v;
+            edges[edges_idx++] = v;
         }
-        assert(edges_idx-pre==deg[i]);
+        assert(edges_idx - pre == deg[i]);
     }
     delete graph;
-    graph=new Graph();
-    graph->init_with_basic(new_n,new_m,deg,pstart,edges);
+    graph = new Graph();
+    graph->init_with_basic(new_n, new_m, deg, pstart, edges);
     delete[] id_map;
     delete[] pstart;
     delete[] edges;
     delete[] deg;
 }
-    
 
-int main(int argc, char *argv[]) {
-	printf("\n-----------------------------------------------------------------------------------------\n");
-	
-	file_path=string(argv[1]);
-    cout<<file_path<<endl;
-	gama=atof(argv[2]);
-    
-	cout<<"gama: "<<gama<<endl;
-    if(gama<0.5){
-        cout<<"error! gamma must >=0.5"<<endl;
+int main(int argc, char *argv[])
+{
+    printf("\n-----------------------------------------------------------------------------------------\n");
+
+    file_path = string(argv[1]);
+    cout << file_path << endl;
+    gama = atof(argv[2]);
+
+    cout << "gama: " << gama << endl;
+    if (gama < 0.5)
+    {
+        cout << "error! gamma must >=0.5" << endl;
         exit(1);
     }
-	double alpha = 1-gama;
-    graph=new Graph();
+    double alpha = 1 - gama;
+    graph = new Graph();
     graph->readFromFile(file_path);
-    //If you are not sure whether the graph input is valid, please let the following row check it.
-    //check_graph(graph);
+    // If you are not sure whether the graph input is valid, please let the following row check it.
+    // check_graph(graph);
     long long s1 = clock();
 
-    cout<<filename<<" n:"<<graph->n<<" m:"<<graph->m<<endl;
-	SuperUB_Extractor se;
-    se.init(file_path,gama);
+    cout << filename << " n:" << graph->n << " m:" << graph->m << endl;
+    SuperUB_Extractor se;
+    se.init(file_path, gama);
 
-    long long start=clock();
-    int lb,n=graph->n,k;
+    long long start = clock();
+    int lb, n = graph->n, k;
     bool need_iteration = true;
 #ifndef NO_PRE_PROC
-    se.get_quasi_bound(lb,n);
-    cout<<"Extract UB:"<<(double)(clock()-start)/CLOCKS_PER_SEC<<"s UB value:"<<n<<" LB value:"<<
-    lb<<endl;
+    se.get_quasi_bound(lb, n);
+    cout << "Extract UB:" << (double)(clock() - start) / CLOCKS_PER_SEC << "s UB value:" << n << " LB value:" << lb << endl;
 
-	k=1+alpha*(n-1);
-    start=clock();
+    k = 1 + alpha * (n - 1);
+    start = clock();
     MiniGraph mini_g;
-    assert(graph->edge_to!=nullptr);
-    mini_g.init(graph->n,graph->m,graph->pstart,graph->edge_to);
-    int pre_graph_n=graph->n,pre_graph_m=graph->m;
-    if(lb>0) shrink_graph(mini_g,my_floor((lb-1)*gama)+1); 
-    cout<<"Prunning graph:"<<endl<< "new n:"<<mini_g.n<<" "<<"new m:"<<mini_g.m<<endl;
-    printf("Prunning N:%.2f%  M:%.2f%\n",100*(double)(pre_graph_n-mini_g.n)/pre_graph_n,100*(double)(pre_graph_m-mini_g.m)/pre_graph_m);
-    cout<<"Shrink graph Cost:"<<(double)(clock()-start)/CLOCKS_PER_SEC<<endl;
-    n=min(max(mini_g.n,lb),n);
-    need_iteration = (mini_g.n!=0&&mini_g.n>lb);
+    assert(graph->edge_to != nullptr);
+    mini_g.init(graph->n, graph->m, graph->pstart, graph->edge_to);
+    int pre_graph_n = graph->n, pre_graph_m = graph->m;
+    if (lb > 0)
+        shrink_graph(mini_g, my_floor((lb - 1) * gama) + 1);
+    cout << "Prunning graph:" << endl
+         << "new n:" << mini_g.n << " " << "new m:" << mini_g.m << endl;
+    printf("Prunning N:%.2f%  M:%.2f%\n", 100 * (double)(pre_graph_n - mini_g.n) / pre_graph_n, 100 * (double)(pre_graph_m - mini_g.m) / pre_graph_m);
+    cout << "Shrink graph Cost:" << (double)(clock() - start) / CLOCKS_PER_SEC << endl;
+    n = min(max(mini_g.n, lb), n);
+    need_iteration = (mini_g.n != 0 && mini_g.n > lb);
 #endif
-    start=clock();
-    if(need_iteration){
-        k=my_floor(1+alpha*(n-1));
-        while(1){
-            int UB=n;
+    start = clock();
+    if (need_iteration)
+    {
+        k = my_floor(1 + alpha * (n - 1));
+        while (1)
+        {
+            int UB = n;
             std::cout << "UB: " << std::left << std::setw(6) << UB << " ";
             std::cout << "K: " << std::left << std::setw(6) << k << " ";
-            bool is_bad_instance=true;
-            int pre_n=n;
-            n=search(graph,k,gama,n,is_bad_instance);
+            bool is_bad_instance = true;
+            int pre_n = n;
+            n = search(graph, k, gama, n, is_bad_instance);
             std::cout << "N: " << std::left << std::setw(6) << n << " ";
-            if(k==my_floor(alpha*(n-1)+1)&&!is_bad_instance) break;
-            k=my_floor(alpha*(n-1)+1);
+            if (k == my_floor(alpha * (n - 1) + 1) && !is_bad_instance)
+                break;
+            k = my_floor(alpha * (n - 1) + 1);
             puts("");
         }
     }
-    cout<<"Final Iteration Cost:"<<(double)(clock()-start)/CLOCKS_PER_SEC<<endl;
+    cout << "Final Iteration Cost:" << (double)(clock() - start) / CLOCKS_PER_SEC << endl;
     // vector<int> res;
     // for(auto v:solution) res.push_back(v);
     delete graph;
-	cout<<"Running Time: "<<(double)(clock()-s1)/CLOCKS_PER_SEC<<"s"<<endl;
-	cout<<"res: "<<n<<endl;
+    cout << "Running Time: " << (double)(clock() - s1) / CLOCKS_PER_SEC << "s" << endl;
+    cout << "res: " << n << endl;
     // for(auto v:solution) cout<<v<<" ";
     // cout<<endl;
-	printf("-----------------------------------------------------------------------------------------\n\n");
+    printf("-----------------------------------------------------------------------------------------\n\n");
 }
-//n-1-core=alpha*(n-1)
-//gama*n=core-alpha+1
-//n=(core-alpha+1)/gama=core/gama+1
+// n-1-core=alpha*(n-1)
+// gama*n=core-alpha+1
+// n=(core-alpha+1)/gama=core/gama+1
